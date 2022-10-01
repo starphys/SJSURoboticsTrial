824f7d9858d54e892aba81a2459628bc8a563382
Evan Astle <starphys@gmail.com>
2022-10-01 15:29:35 -0700

    Calculating pitch and roll from accelerometers (#1)
    * Writing to serial, but only displaying -1 so far
    
    * basic communication, not working yet
    
    * Part working, getting accelerometer data!
    
    * Calculating pitch and roll from accelerometers

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,5 +1,49 @@
+/*
+  Most code in this is modified from Arduino and MPU6050 Accelerometer 
+  and Gyroscope Sensor Tutorial by Dejan, https://howtomechatronics.com
+*/
+#include <Wire.h>
+
+const int MPU = 0x68;  // MPU6050 I2C address
+
+float AccX, AccY, AccZ;
+
 void setup() {
+  Serial.begin(19200);
+
+  //reset and wakeup device
+  Wire.begin();                 // Initialize comunication
+  Wire.beginTransmission(MPU);  // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);             // Talk to the register 6B
+  Wire.write(0x00);             // Set reset bit in register 6B to 1
+  Wire.endTransmission(true);   //end the transmission
 }
 
 void loop() {
-  }
+
+  //Accelerometer data stored in 6 contiguous registers in 3 sets of 2 starting with 3B
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  //(ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);  // Read 6 registers total, each axis value is stored in 2 registers
+
+  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0;  // X-axis value
+  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0;  // Y-axis value
+  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
+
+//http://robo.sntiitk.in/2017/12/21/Beginners-Guide-to-IMU.html
+  float roll = (180 * atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2)))/ PI);
+  float pitch = (180 * atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2)))/ PI);
+
+  Serial.print("AccX: ");
+  Serial.print(AccX);
+  Serial.print(", AccY: ");
+  Serial.print(AccY);
+  Serial.print(", AccZ: ");
+  Serial.print(AccZ);
+  Serial.print(", roll: ");
+  Serial.print(roll);
+  Serial.print(", pitch: ");
+  Serial.print(pitch);
+  Serial.print("\n");
+}
